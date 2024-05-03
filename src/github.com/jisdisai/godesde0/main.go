e34b23a5e1801cdbf76f5d64a18383d39996f2e1
Isai Salgado <joaquin.salgado@ymail.com>
2024-05-02 23:05:28 -0600

    subiendo cambios de estado, texto

--- src/github.com/jisdisai/godesde0/main.go
+++ src/github.com/jisdisai/godesde0/main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/jisdisai/godesde0/variables"
 )
 
 func main() {
-	variables.MuestroEnteros()
+	//variables.MuestroEnteros()
+	//variables.RestoVariables()
+
+	estado, texto := variables.ConviertoaTexto(1588)
+	fmt.Println(estado, texto)
+
 }
 
 // correr un programa en go es go run main.go
